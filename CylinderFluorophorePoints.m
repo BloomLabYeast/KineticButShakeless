e078fd5f9a0c6da1f54703b937da2efc802a3ed8
AyushDoshi <ayushdoshi1@gmail.com>
2018-07-25 07:49:51 -0400

    Added XML writer and main base structure builder

--- CylinderFluorophorePoints.m
+++ CylinderFluorophorePoints.m
@@ -1,19 +1,22 @@
-function CoordPoints = CylinderFluorophorePoints(NumberOfPoints,Length, randangle, randanglelower, randangleupper, SetAngle, SetXAngle, SetYAngle, SetZAngle)
+function CylinderCoordPoints = CylinderFluorophorePoints(NumberOfPoints,Length, randangle, randanglelower, randangleupper, SetAngle, SetXAngle, SetYAngle, SetZAngle)
 
 TubuleDiameter = 25;
 
-CoordPoints = zeros(3,NumberOfPoints);
+CylinderCoordPoints = zeros(3,NumberOfPoints);
 for PointIdx = 1:NumberOfPoints
-    CoordPoints(1,PointIdx) = Length*(rand()-0.5);
+    CylinderCoordPoints(1,PointIdx) = Length*(rand()-0.5);
     CoordAng = rand()*2*pi;
-    CoordPoints(2,PointIdx) = cos(CoordAng)*TubuleDiameter/2;
-    CoordPoints(3,PointIdx) = sin(CoordAng)*TubuleDiameter/2;
+    CylinderCoordPoints(2,PointIdx) = cos(CoordAng)*TubuleDiameter/2;
+    CylinderCoordPoints(3,PointIdx) = sin(CoordAng)*TubuleDiameter/2;
 end
 
 randanglediff = randangleupper - randanglelower;
 
 if randangle == 1
-    CoordPoints = rotx(rand()*randanglediff+randanglelower)*roty(rand()*randanglediff+randanglelower)*rotz(rand()*randanglediff+randanglelower)*CoordPoints;
+    CylinderCoordPoints = rotx(rand()*randanglediff+randanglelower)*...
+        roty(rand()*randanglediff+randanglelower)*...
+        rotz(rand()*randanglediff+randanglelower)*CylinderCoordPoints;
 elseif SetAngle == 1
-    CoordPoints = rotx(SetXAngle)*roty(SetYAngle)*rotz(SetZAngle)*CoordPoints;
+    CylinderCoordPoints = rotx(SetXAngle)*roty(SetYAngle)*...
+        rotz(SetZAngle)*CylinderCoordPoints;
 end
