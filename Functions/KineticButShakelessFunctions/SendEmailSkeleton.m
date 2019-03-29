9bfe0144c41712e7f389ea024341b9fe34828ad6
Ayush Doshi <ayushdoshi1@gmail.com>
2019-03-28 22:32:08 -0400

    Fixed SendEmailSkeleton.m

--- Functions/KineticButShakelessFunctions/SendEmailSkeleton.m
+++ Functions/KineticButShakelessFunctions/SendEmailSkeleton.m
@@ -2,5 +2,5 @@ function SendEmailSkeleton(purpose,emailaddress,string)
     gmail_user = %Add Gmail username here
     gmail_password = %Add Gmail password here
     string = purpose + ": " + string;
-    system(sprintf('python SendEmail.py %s %s %s "%s"', gmail_user, gmail_password, emailaddress, string));
+    system(sprintf('python %s %s %s %s "%s"', [pwd filesep 'Functions' filesep 'KineticButShakelessFunctions' filesep 'SendEmail.py'], gmail_user, gmail_password, emailaddress, string));
 end
