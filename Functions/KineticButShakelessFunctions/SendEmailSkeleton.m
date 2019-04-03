02ba93d3516fb891c345b441ddc47e0f6ff12bda
Ayush Doshi <ayushdoshi1@gmail.com>
2019-04-02 22:09:39 -0400

    Fixed path problem with SendEmailSkeleton.m

--- Functions/KineticButShakelessFunctions/SendEmailSkeleton.m
+++ Functions/KineticButShakelessFunctions/SendEmailSkeleton.m
@@ -2,5 +2,6 @@ function SendEmailSkeleton(purpose,emailaddress,string)
     gmail_user = %Add Gmail username here
     gmail_password = %Add Gmail password here
     string = purpose + ": " + string;
-    system(sprintf('python %s %s %s %s "%s"', [pwd filesep 'Functions' filesep 'KineticButShakelessFunctions' filesep 'SendEmail.py'], gmail_user, gmail_password, emailaddress, string));
+    [path,~,~] = fileparts(which('SendEmail.m'));
+    system(sprintf('python %s %s %s %s "%s"', [path filesep 'SendEmail.py'], gmail_user, gmail_password, emailaddress, string));
 end
